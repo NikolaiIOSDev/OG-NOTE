04544f3f85f9c04b69b55153777e6d4d47a0029e
Nikolai <nickolay.fedorov.iosdev@gmail.com>
2023-03-02 15:33:40 +0300

    Rename Project

--- OG NOTE/Modules/Splash Screen/SplashViewController.swift
+++ OG NOTE/Modules/Splash Screen/SplashViewController.swift
@@ -8,14 +8,11 @@
 import UIKit
 
 class SplashViewController: UIViewController {
-    @IBOutlet var cftLabel:UILabel!
     @IBOutlet var logoLabel:UILabel!
     
     @IBOutlet var constraintLogo: NSLayoutConstraint!
-    @IBOutlet var constraintCft: NSLayoutConstraint!
     
     @IBOutlet var constraintLogoBottom: NSLayoutConstraint!
-    @IBOutlet var constraintCftBottom: NSLayoutConstraint!
     
     
     private var timer = Timer()
@@ -31,18 +28,14 @@ class SplashViewController: UIViewController {
     
     //MARK: - Setting for Label
     private func settinglabel(){
-        cftLabel.font = UIFont(name: FontsConstant.fonts, size: 60)
         logoLabel.font = UIFont(name: FontsConstant.fonts, size: 50)
         logoLabel.text = "OG NOTE"
-        
-        cftLabel.textColor = UIColor(named: ColorConstant.SplashModule.colorLabelSplash)
         logoLabel.textColor = UIColor(named: ColorConstant.SplashModule.colorLabelSplash)
     }
     
     
     //MARK: - Animations for Label
     private func animations(){
-        constraintCft.constant = 0
         constraintLogo.constant = 0
         UIView.animate(withDuration: 1.0,
                        delay: 0.3,
@@ -59,7 +52,6 @@ class SplashViewController: UIViewController {
     }
     
     @objc private func animationsBottom(){
-        constraintCftBottom.constant = -1000
         constraintLogoBottom.constant = -1000
         UIView.animate(withDuration: 1.0,
                        delay: 0.3,
